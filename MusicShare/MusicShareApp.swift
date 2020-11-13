00189babb904674b822a8e26f2c263bf13e2211c
Carson <car.grin89@gmail.com>
2020-11-12 21:47:02 -0500

    Initial Commit

---
+++ MusicShare/MusicShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicShareApp.swift
+//  MusicShare
+//
+//  Created by Carson Grin on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MusicShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
